0ace5f0c414f03f6c1ee5706a2ce4dd33aec8f6a
Aleksandr <sanyamaks14@gmail.com>
2020-11-25 16:41:49 +0400

    Fix TextLogo

--- src/components/TextLogo/TextLogo.js
+++ src/components/TextLogo/TextLogo.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import cn from 'classnames';
+import { Link } from 'react-router-dom';
 import './TextLogo.css';
 
-const TextLogo = () => {
-  return <p className="text-logo">NewsExplorer</p>;
+const TextLogo = (props) => {
+  const { modifier } = props;
+  return (
+    <Link
+      className={cn('text-logo', { 'text-logo_white': modifier === 'white' })}
+      to="/"
+    >
+      NewsExplorer
+    </Link>
+  );
 };
 
 export default TextLogo;
