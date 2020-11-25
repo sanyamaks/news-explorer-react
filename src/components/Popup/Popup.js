f65973cd01620659b41c3f7e8384013674b138be
Aleksandr <sanyamaks14@gmail.com>
2020-11-25 16:29:23 +0400

    Add Popup

---
+++ src/components/Popup/Popup.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Popup.css';
+import { ReactComponent as Icon } from '../../assets/icons/icon-close-popup.svg';
+import FormSignIn from '../Form/FormSignIn/FormSignIn';
+import FormSignUp from '../Form/FormSignUp/FormSignUp';
+import Notification from '../Notification/Notification';
+
+const Popup = (props) => {
+  console.log('Popup');
+
+  const { isActive, closePopup, children } = props;
+  if (!isActive) {
+    return null;
+  }
+
+  const popupClick = (event) => {
+    if (event.target === document.querySelector('.popup')) {
+      closePopup();
+    }
+  };
+
+  return ReactDOM.createPortal(
+    <div className="popup" onClick={popupClick}>
+      <div className="popup__content">
+        <button
+          className="popup__button-close"
+          type="button"
+          onClick={closePopup}
+        >
+          <Icon className="popup__icon-close" />
+        </button>
+        {children}
+        {/*<FormSignIn />*/}
+        {/*<FormSignUp />*/}
+        {/*<Notification*/}
+        {/*  text="Пользователь успешо зарегистирован"*/}
+        {/*  buttonText="Войти"*/}
+        {/*/>*/}
+      </div>
+    </div>,
+    document.getElementById('root-popup')
+  );
+};
+
+export default React.memo(Popup);
