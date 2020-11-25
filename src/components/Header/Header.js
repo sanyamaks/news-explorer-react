0bde5a1d18aff1154332a0cf7174725b214480de
Aleksandr <sanyamaks14@gmail.com>
2020-11-25 16:52:21 +0400

    Header. Add logic for NavWrapper. Add modifier for all components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,33 +1,55 @@
 import React, { useState } from 'react';
+import cn from 'classnames';
 import './Header.css';
 import TextLogo from '../TextLogo/TextLogo';
 import NavbarButton from '../NavbarButton/NavbarButton';
 import Nav from '../Nav/Nav';
 import Button from '../Button/Button.js';
 import { ReactComponent as Icon } from '../../assets/icons/Union_black.svg';
+import Notification from '../Notification/Notification';
+import FormSignUp from '../Form/FormSignUp/FormSignUp';
 
 const Header = (props) => {
-  const { modifier } = props;
+  const { modifier, handleClick } = props;
 
-  const handleClick = () => {
-    setButtonProps({ ...buttonProps, variant: 'blue' });
+  const [isActiveNavWrapper, setActiveNavWrapper] = useState(false);
+
+  const toggleNavWrapper = () => {
+    setActiveNavWrapper(!isActiveNavWrapper);
   };
 
-  const [buttonProps, setButtonProps] = useState({
-    className: 'header__button',
-    text: 'Регистрация',
-    variant: 'outline-white',
-    onClick: handleClick,
-    Icon: Icon,
-  });
+  const resetNavWrapper = () => {
+    setActiveNavWrapper(false);
+  };
+
+  const onClick = () => {
+    resetNavWrapper();
+    if (handleClick) {
+      return handleClick();
+    }
+  };
 
   return (
-    <header className="header">
-      <TextLogo />
-      <NavbarButton modifier={modifier} />
-      <div className="header__nav-wrapper">
-        <Nav modifier="white" />
-        <Button {...buttonProps} />
+    <header
+      className={cn('header', modifier === 'white' ? 'header_white' : null)}
+    >
+      <TextLogo modifier={modifier} />
+      <NavbarButton modifier={modifier} onClick={toggleNavWrapper} />
+      <div
+        className={cn(
+          'header__nav-wrapper',
+          isActiveNavWrapper ? 'header__nav-wrapper_enabled' : null,
+          modifier === 'white' ? 'header__nav-wrapper_white' : null
+        )}
+      >
+        <Nav modifier={modifier} onClick={resetNavWrapper} />
+        <Button
+          className="header__button"
+          text="Регистрация"
+          variant={modifier === 'white' ? 'outline-white' : null}
+          onClick={onClick}
+          Icon={Icon}
+        />
       </div>
     </header>
   );
